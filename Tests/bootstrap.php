615ba41a1ecd8bc71f7dd3ad3f9e613646ae46cf
Thomas Uhlmann <tho.uhlmann@gmail.com>
2016-02-03 19:12:28 +0100

    Delete cache before tests

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -4,6 +4,7 @@
 use Doctrine\Common\Annotations\AnnotationRegistry;
 use Composer\Autoload\ClassLoader;
 
+rrmdir(__DIR__ . '/Dummy/app/cache');
 error_reporting(E_ALL | E_STRICT);
 
 /**
@@ -14,3 +15,20 @@ $loader = require __DIR__ . '/../vendor/autoload.php';
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
 return $loader;
+
+// To remove the cache dir
+function rrmdir($dir)
+{
+    if (is_dir($dir)) {
+        $objects = scandir($dir);
+        foreach ($objects as $object) {
+            if ($object != "." && $object != "..") {
+                if (is_dir($dir . "/" . $object))
+                    rrmdir($dir . "/" . $object);
+                else
+                    unlink($dir . "/" . $object);
+            }
+        }
+        rmdir($dir);
+    }
+}
