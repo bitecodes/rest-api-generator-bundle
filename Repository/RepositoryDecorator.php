34c054ab6cb60a08b736bf0822594fb09dc928fa
Thomas Uhlmann <tho.uhlmann@gmail.com>
2016-02-03 20:18:24 +0100

    Add RepositoryDecorator

---
+++ Repository/RepositoryDecorator.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Fludio\RestApiGeneratorBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use Fludio\DoctrineFilter\Traits\EntityFilterTrait;
+
+class RepositoryDecorator
+{
+    use EntityFilterTrait;
+
+    /**
+     * @var EntityRepository
+     */
+    private $baseRepository;
+
+    public function __construct(EntityRepository $repository)
+    {
+        $this->baseRepository = $repository;
+    }
+
+    public function __call($method, $arguments)
+    {
+        if (!method_exists($this->baseRepository, $method)) {
+            $message = sprintf("Undefined method %s in class %s.", $method, get_class($this->baseRepository));
+            throw new Exception($message);
+        }
+
+        return call_user_func_array(array($this->baseRepository, $method), $arguments);
+    }
+}
