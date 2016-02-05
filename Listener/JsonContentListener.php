0e062cd7d0f38b5edd092cb072ebfbc31e03d446
Thomas Uhlmann <tho.uhlmann@gmail.com>
2016-02-05 15:17:29 +0100

    Validate json decoding

--- Listener/JsonContentListener.php
+++ Listener/JsonContentListener.php
@@ -2,6 +2,8 @@
 
 namespace Fludio\RestApiGeneratorBundle\Listener;
 
+use Fludio\RestApiGeneratorBundle\Exception\ApiProblem;
+use Fludio\RestApiGeneratorBundle\Exception\ApiProblemException;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 
 class JsonContentListener
@@ -12,7 +14,35 @@ class JsonContentListener
 
         if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
             $data = json_decode($request->getContent(), true);
+            if (!$data) {
+                $this->validateJsonContent();
+            }
             $request->request->replace(is_array($data) ? $data : []);
+        }
+    }
+
+    protected function validateJsonContent()
+    {
+        switch (json_last_error()) {
+            case JSON_ERROR_DEPTH:
+                $error = 'Maximum stack depth exceeded';
+                break;
+            case JSON_ERROR_CTRL_CHAR:
+                $error = 'Unexpected control character found';
+                break;
+            case JSON_ERROR_SYNTAX:
+                $error = 'Syntax error, malformed JSON';
+                break;
+        }
+
+        if (!empty($error)) {
+            $problem = new ApiProblem(
+                400,
+                ApiProblem::TYPE_INVALID_REQUEST_BODY_FORMAT
+            );
+            $problem->set('error', $error);
+
+            throw new ApiProblemException($problem);
         }
     }
 }
