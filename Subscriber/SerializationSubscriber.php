5de890546f3bc6e533b4a80d5168157212831831
Thomas Uhlmann <tho.uhlmann@gmail.com>
2016-02-07 14:54:06 +0100

    Make SerializationListener a subscriber

--- Subscriber/SerializationSubscriber.php
+++ Subscriber/SerializationSubscriber.php
@@ -1,34 +1,60 @@
 <?php
 
-namespace Fludio\RestApiGeneratorBundle\Listener;
+namespace Fludio\RestApiGeneratorBundle\Subscriber;
 
 use Fludio\RestApiGeneratorBundle\Api\ApiResponse;
 use Fludio\RestApiGeneratorBundle\Controller\RestApiController;
 use JMS\Serializer\Serializer;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
 use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
 
-class SerializationListener
+class SerializationSubscriber implements EventSubscriberInterface
 {
     /**
      * @var Serializer
      */
     private $serializer;
+    /**
+     * @var bool
+     */
     private $isRestController = false;
 
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::CONTROLLER => 'checkForControllerType',
+            KernelEvents::VIEW => 'serializeResponse'
+        ];
+    }
+
+    /**
+     * SerializationSubscriber constructor.
+     * @param Serializer $serializer
+     */
     public function __construct(Serializer $serializer)
     {
         $this->serializer = $serializer;
     }
 
-    public function onKernelController(FilterControllerEvent $event)
+    /**
+     * @param FilterControllerEvent $event
+     */
+    public function checkForControllerType(FilterControllerEvent $event)
     {
         $controller = $event->getController()[0];
         $this->isRestController = $controller instanceof RestApiController;
     }
 
-    public function onKernelView(GetResponseForControllerResultEvent $event)
+    /**
+     * @param GetResponseForControllerResultEvent $event
+     */
+    public function serializeResponse(GetResponseForControllerResultEvent $event)
     {
         if ($this->isRestController) {
             $data = $event->getControllerResult();
