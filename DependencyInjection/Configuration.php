bd721b83fb81436fba131bc8846ea024fd36e2f2
Thomas Uhlmann <tho.uhlmann@gmail.com>
2016-03-04 23:39:32 +0100

    Remove unused import

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -3,7 +3,6 @@
 namespace Fludio\RestApiGeneratorBundle\DependencyInjection;
 
 use Fludio\RestApiGeneratorBundle\Form\DynamicFormType;
-use Fludio\RestApiGeneratorBundle\DependencyInjection\ConfigurationProcessor;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\BooleanNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\ScalarNodeDefinition;
